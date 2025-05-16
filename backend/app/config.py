1cc1361ecbe52001a2025c4b8a2a5c38b07699b6
Jakubo0451 <jakubo@stud.ntnu.no>
2025-05-16 15:42:33 +0200

    swtiching db url to a vps, trial time ran out on render!

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,5 @@
-DATABASE_URL = "postgresql://yapperdb_user:GKwNgaw5YEyyYPSCCG3hVIayOmr90vvp@dpg-cvrqj7a4d50c73d3rimg-a.frankfurt-postgres.render.com:5432/yapperdb"
+DATABASE_URL="postgresql://admin:TZwcOLyfnM@2.56.164.114:5432/yapper" #we switched the database to a vps because we ran out of trial time on render.com
+#DATABASE_URL = "postgresql://yapperdb_user:GKwNgaw5YEyyYPSCCG3hVIayOmr90vvp@dpg-cvrqj7a4d50c73d3rimg-a.frankfurt-postgres.render.com:5432/yapperdb"
 #DATABASE_URL = "postgresql://postgres:jacob123@localhost:5432/YapperDB"
 SECRET_KEY = "your_secret_key"  
 ALGORITHM = "HS256" 
